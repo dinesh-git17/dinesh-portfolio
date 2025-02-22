13c54bd30e2b356e11bbda3b1fd25d0616c45204
dinesh-git17 <dineshddawo@gmail.com>
2025-02-21 19:59:16 -0500

    seo stuff

---
+++ next-seo.config.js
@@ -0,0 +1,19 @@
+// next-seo.config.js
+const seoConfig = {
+  title: "Dinesh Dawonauth | Data Scientist & Developer",
+  description:
+    "Showcasing projects and skills in data science and development.",
+  openGraph: {
+    type: "website",
+    locale: "en_CA",
+    url: "https://yourdomain.com/",
+    site_name: "Dinesh Dawonauth Portfolio",
+  },
+  twitter: {
+    handle: "@yourhandle",
+    site: "@yourhandle",
+    cardType: "summary_large_image",
+  },
+};
+
+export default seoConfig;
